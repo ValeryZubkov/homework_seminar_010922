45d596e74dfc37bd74b501ef33d31728d4a757d8
Valery Zubkov <bouddha@mail.ru>
2022-09-05 20:13:31 +0400

    homework number 1

---
+++ HOMEWORK_001/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать натуральные числа от N до 1, N задано
+void Numb(int n)
+{
+    if(n >= 1)
+    {
+    Console.Write($"{n} ");
+    Numb(n-1);
+    }
+}
+Console.WriteLine("Введите число N для отображения обратной последовательности: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+Numb(number);
+Console.WriteLine();
