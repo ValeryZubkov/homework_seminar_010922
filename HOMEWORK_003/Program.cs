7ffeb2b280440e93e5f38587bb28df402a2a25e4
Valery Zubkov <bouddha@mail.ru>
2022-09-06 00:01:03 +0400

    homework number 3

---
+++ HOMEWORK_003/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+int NumbAmount(int m, int n)
+{
+    if(m == n) return n;     
+    if(m < n) return m + NumbAmount(m+1, n);
+    else return m + NumbAmount(m-1, n);
+}
+Console.WriteLine("Введите число M: ");
+int number1 = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите число N: ");
+int number2 = int.Parse(Console.ReadLine() ?? "0");
+Console.Write($"Сумма элементов от {number1} до {number2} составляет: {NumbAmount(number1, number2)}.");
+Console.WriteLine();
