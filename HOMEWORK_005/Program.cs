437735f29bbe1c36aae064f6375022e6000dd33a
Valery Zubkov <bouddha@mail.ru>
2022-09-06 15:46:26 +0400

    homework number 5

---
+++ HOMEWORK_005/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу вычисления функции Аккермана
+int FunctionAck(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else if (m > 0 && n == 0) return FunctionAck(m - 1, 1);
+  else if(m > 0 && n > 0) return FunctionAck(m - 1, FunctionAck(m, n - 1));
+  else return n+1;
+}
+Console.WriteLine("Введите 1-й параметр функции Аккермана(неотрицательное целое число) m : ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите 2-й параметр функции Аккермана(неотрицательное целое число) n : ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"При заданных параметрах функция Аккермана A(m,n) = {FunctionAck(m,n)}. ");
+
+
+
+
