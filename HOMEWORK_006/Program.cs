9e30c90d375267479d87393f955820c2b6594b22
Valery Zubkov <bouddha@mail.ru>
2022-09-06 16:10:45 +0400

    homework number 6

---
+++ HOMEWORK_006/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу возведения числа А в целую степень B
+int NumberInStep(int numb, int step)
+{
+  if(step > 0) return numb * NumberInStep(numb, step-1);
+  else return 1; 
+}
+Console.WriteLine("Введите число А: ");
+int a = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите число B: ");
+int b = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Число {a} в степени {b} = {NumberInStep(a,b)}. ");
