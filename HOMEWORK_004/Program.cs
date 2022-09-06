70133533ca4fb7856b8ce6956b68b2976dbcf451
Valery Zubkov <bouddha@mail.ru>
2022-09-06 14:54:35 +0400

    homework number 4

---
+++ HOMEWORK_004/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму цифр числа
+
+int AmountDigits(int numb)
+{
+    if(numb > 0) return numb%10 + AmountDigits(numb/10);
+    if(numb < 0) return -numb%10 + AmountDigits(-numb/10);
+    else return 0;
+}
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Сумма цифр числа {number} составляет: {AmountDigits(number)}.");
